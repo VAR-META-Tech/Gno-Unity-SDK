05b5cc338275abc4dc4bcdca8817fbce8d058473
baodangvar <baodang@var-meta.com>
2023-12-08 10:18:15 +0700

    update UI

--- UnitySample/Assets/Scripts/modeWallet.cs
+++ UnitySample/Assets/Scripts/modeWallet.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class modeWallet : MonoBehaviour
 {
-    DropdownSample Dropdown;
+    public TextMeshProUGUI txtButtonWallet;
+    public TextMeshProUGUI txtTitleWallet;
+    public GameObject dropDownName;
+
     public void ModeWallet( )
     {
-        int a = 0;
+        int valueOption = dropDownName.GetComponent<TMP_Dropdown>().value;
+        if(valueOption == 0)
+        {
+            txtButtonWallet.text = "Import Wallet";
+            txtTitleWallet.text = "Import Wallet";
+        }
+        if (valueOption == 1)
+        {
+            txtButtonWallet.text = "Create Wallet";
+            txtTitleWallet.text = "Create Wallet";
+        }
     }
 }
